a6694b191eefe8115affc65eebfd051e2712845b
w12495 <w12495>
2009-11-12 03:24:27 +0000

    Applied Eclipse auto format.

--- src/java/au/com/sensis/mobile/web/component/map/model/MapState.java
+++ src/java/au/com/sensis/mobile/web/component/map/model/MapState.java
@@ -6,62 +6,63 @@ import au.com.sensis.address.WGS84Point;
 import au.com.sensis.wireless.manager.mapping.MobilesBoundingBox;
 
 /**
- * Encapsulates the current state of the map so that we can subsequently perform operations on it, such as panning and zooming.
- * This state does not include the map image URL, which can be derived from the state. 
- * 
+ * Encapsulates the current state of the map so that we can subsequently perform
+ * operations on it, such as panning and zooming. This state does not include
+ * the map image URL, which can be derived from the state.
+ *
  * @author Adrian.Koh2@sensis.com.au
  */
 public class MapState {
 
-	/**
-	 * Centre of the map.
-	 */
-	private WGS84Point coordinates;
-	
-	/**
-	 * Bounding box state is required if you subsequently wish to pan the map.
-	 */
-	private MobilesBoundingBox mapBoundingBox;
-	
-	/**
-	 * Zoom level state is required if you subsequently wish to zoom the map.
-	 */
-	private int zoomLevel;
+    /**
+     * Centre of the map.
+     */
+    private final WGS84Point coordinates;
 
-	public MapState(WGS84Point coordinates,
-			MobilesBoundingBox mapBoundingBox, int zoomLevel) {
-		this.coordinates = coordinates;
-		this.mapBoundingBox = mapBoundingBox;
-		this.zoomLevel = zoomLevel;
-	}
+    /**
+     * Bounding box state is required if you subsequently wish to pan the map.
+     */
+    private final MobilesBoundingBox mapBoundingBox;
 
-	/**
-	 * @return the coordinates
-	 */
-	public WGS84Point getCoordinates() {
-		return coordinates;
-	}
+    /**
+     * Zoom level state is required if you subsequently wish to zoom the map.
+     */
+    private final int zoomLevel;
 
-	/**
-	 * @return the mapBoundingBox
-	 */
-	public MobilesBoundingBox getMapBoundingBox() {
-		return mapBoundingBox;
-	}
+    public MapState(final WGS84Point coordinates,
+            final MobilesBoundingBox mapBoundingBox, final int zoomLevel) {
+        this.coordinates = coordinates;
+        this.mapBoundingBox = mapBoundingBox;
+        this.zoomLevel = zoomLevel;
+    }
 
-	/**
-	 * @return the zoomLevel
-	 */
-	public int getZoomLevel() {
-		return zoomLevel;
-	}
+    /**
+     * @return the coordinates
+     */
+    public WGS84Point getCoordinates() {
+        return coordinates;
+    }
 
-	/**
-	 * {@inheritDoc}
-	 */
-	@Override
-	public String toString() {
+    /**
+     * @return the mapBoundingBox
+     */
+    public MobilesBoundingBox getMapBoundingBox() {
+        return mapBoundingBox;
+    }
 
-		return ToStringBuilder.reflectionToString(this);
-	}
+    /**
+     * @return the zoomLevel
+     */
+    public int getZoomLevel() {
+        return zoomLevel;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+
+        return ToStringBuilder.reflectionToString(this);
+    }
 }
