9532785afcc55450d7e0647b582f37e56e7b06a1
w12495 <w12495>
2010-02-09 03:30:40 +0000

    Formatted to remove checkstyle warnings.

--- showcase/java/au/com/sensis/mobile/web/component/map/showcase/presentation/form/MapForm.java
+++ showcase/java/au/com/sensis/mobile/web/component/map/showcase/presentation/form/MapForm.java
@@ -2,34 +2,41 @@ package au.com.sensis.mobile.web.component.map.showcase.presentation.form;
 
 import org.apache.commons.lang.StringUtils;
 
+/**
+ * Struts2 form object for maps.
+ *
+ * @author Adrian.Koh2@sensis.com.au
+ */
 public class MapForm {
 
-	private String location;
+    private String location;
 
-	/**
-	 * @return the location
-	 */
-	public String getLocation() {
-		if (location == null) {
-			return StringUtils.EMPTY;
-		}
-		return location;
-	}
+    /**
+     * @return the location
+     */
+    public String getLocation() {
+        if (location == null) {
+            return StringUtils.EMPTY;
+        }
+        return location;
+    }
 
-	/**
-	 * @param location the location to set
-	 */
-	public void setLocation(String location) {
-		this.location = location;
-	}
-	
-	/**
-	 * Shorthand for {@link #setLocation(String)}.
-	 * 
-	 * @param location the location to set
-	 */
-	public void setMl(String location) {
-		setLocation(location);
-	}
-	
+    /**
+     * @param location
+     *            the location to set
+     */
+    public void setLocation(final String location) {
+        this.location = location;
+    }
+
+    /**
+     * Shorthand for {@link #setLocation(String)}.
+     *
+     * @param location
+     *            the location to set
+     */
+    public void setMl(final String location) {
+        setLocation(location);
+    }
+
 }
