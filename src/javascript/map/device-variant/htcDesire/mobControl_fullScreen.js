ca752eb5c6e33fd82e38133248d6b9b6b1736063
w89477 <w89477>
2010-04-13 08:24:13 +0000

    addtional of android devices

---
+++ src/javascript/map/device-variant/htcDesire/mobControl_fullScreen.js
@@ -0,0 +1,11 @@
+EMS.Control.FullScreen = OpenLayers.Class(EMS.Control.FullScreenPrototype, { 
+	
+	fullLandscapeWidth: '800px',
+	fullLandscapeHeight: '450px',
+	
+	fullPortraitWidth: '500px',
+	fullPortraitHeight: '750px',
+	
+});
+
+_MapControls_.push(new EMS.Control.FullScreen);
---
+++ src/javascript/map/device-variant/sex10/mobControl_fullScreen.js
@@ -0,0 +1,11 @@
+EMS.Control.FullScreen = OpenLayers.Class(EMS.Control.FullScreenPrototype, { 
+	
+	fullLandscapeWidth: '565px',
+	fullLandscapeHeight: '275px',
+	
+	fullPortraitWidth: '315px',
+	fullPortraitHeight: '520px',
+	
+});
+
+_MapControls_.push(new EMS.Control.FullScreen);
