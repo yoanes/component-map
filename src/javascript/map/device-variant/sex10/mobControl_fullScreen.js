702d425944c60ed9d9ef04adaac912fa294fc14a
w89477 <w89477>
2010-04-28 02:54:04 +0000

    adjust the height of both portrait and landscape for fullscreen to show map controls above the device's buttons

--- src/javascript/map/device-variant/sex10/mobControl_fullScreen.js
+++ src/javascript/map/device-variant/sex10/mobControl_fullScreen.js
@@ -1,10 +1,10 @@
 EMS.Control.FullScreen = OpenLayers.Class(EMS.Control.FullScreenPrototype, { 
 	
 	fullLandscapeWidth: '565px',
-	fullLandscapeHeight: '275px',
+	fullLandscapeHeight: '225px',
 	
 	fullPortraitWidth: '315px',
-	fullPortraitHeight: '520px',
+	fullPortraitHeight: '480px',
 	
 });
 
