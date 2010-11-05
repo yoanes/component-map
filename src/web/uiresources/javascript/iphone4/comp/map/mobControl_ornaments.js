24b039c7f1f7eaf467ac2b8e68289709696ef354
w12495 <w12495>
2010-11-05 02:45:48 +0000

    Now depends on:
            <dependency org="com.sensis" name="wl-build-scripts" rev="2.0.9" conf="buildBootstrap->default" />
            <dependency org="com.sensis" name="wl-crf" rev="0.1.2-103" conf="default" />
            <dependency org="com.sensis" name="wl-core-component" rev="2.0.4" conf="default,test"/>
    
    Simplified map-crf-config.xml and javascript groups using new wl-crf capability: files with the same name in more specific groups mask less specific groups.

---
+++ src/web/uiresources/javascript/iphone4/comp/map/mobControl_ornaments.js
@@ -0,0 +1,16 @@
+EMS.Control.Ornaments = OpenLayers.Class(EMS.Control.OrnamentsPrototype, {
+	
+	calcDimension: function() {
+		/* set the used images sizes to half of it */
+		this.northImage.width /= 2;
+		this.logoImage.width /= 2;
+		
+		var north = new OpenLayers.Size(this.northImage.width, this.northImage.height);
+		var logo = new OpenLayers.Size(this.logoImage.width, this.logoImage.height);
+		
+		return {'north': north, 'logo': logo};
+	}
+	
+});
+
+_MapControls_.push(new EMS.Control.Ornaments);
