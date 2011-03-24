bdaf9beaa2036788e705329b8540cf8e303e5ced
w89477 <w89477>
2011-03-24 00:57:54 +0000

    android and apple should get interactive poi and location marker

---
+++ src/web/uiresources/javascript/map-apple-android/comp/map/devicelocation.adaptor.js
@@ -0,0 +1,98 @@
+/* Device location instance on map component */
+MAP.DEVICELOCATION = {};
+MAP.DEVICELOCATION.errorMessage = "Sorry, we couldn't locate you.";
+
+/* follow me */
+MAP.DEVICELOCATION.AutoLocate = {};
+MAP.DEVICELOCATION.AutoLocate.onlocate = function(position) {
+	if(position == null) return;
+	
+	/* get distance in px */
+	var panBy = MAP.instances[0].getLocMapCtrPxDiff(position);
+	/* do the pan */
+	MAP.instances[0].Map.pan(panBy.x, panBy.y, {animate:true});
+	/* zoom in if the map is zoom out too much */
+	if(MAP.instances[0].Map.getZoom() < 9)
+		setTimeout(function() {EMS.Util.smoothZoom(MAP.instances[0].Map, MAP.instances[0].Map.getCenter(), userLatLon, 11 );} , 500);
+	
+	/* keep adding location poi */
+	MAP.instances[0].addLocationPoi(position, position.accuracy, true);
+};
+
+MAP.DEVICELOCATION.AutoLocate.prelocate = function() {
+	MAP.instances[0].clearLocationPoi();
+};
+
+MAP.DEVICELOCATION.AutoLocate.postlocate = function(lastRecordedPosition) {
+	if(!$defined(lastRecordedPosition))
+		alert(_DEVICELOCATION_WM_.errorMessage);
+	
+	else MAP.instances[0].addLocationPoi(lastRecordedPosition, lastRecordedPosition.accuracy, false);
+};
+
+MAP.DEVICELOCATION.AutoLocate.onerror = function(error) { 
+	alert(MAP.DEVICELOCATION.errorMessage);
+};
+
+/* limits */
+MAP.DEVICELOCATION.AutoLocate.limits = {};
+MAP.DEVICELOCATION.AutoLocate.limits.tries = 10;
+MAP.DEVICELOCATION.AutoLocate.limits.proximity = 50;
+MAP.DEVICELOCATION.AutoLocate.limits.timeout = 30000;
+MAP.DEVICELOCATION.AutoLocate.limits.acceptableProximity = 1000;
+
+/* extends the MobEMS base class to use addLocationPoi() */
+MobEMS.implement({
+	LocationMarker: null,
+	LocationPoi: null,
+	
+	addLocationPoi: function(coordinates, accuracy, stillLocating) {
+		/* if there's no location poi on the map */
+		if(this.LocationMarker == null) {
+			/* this method expects the family level of device instantiate the LocationPoi correctly */
+			this.LocationMarker = new OpenLayers.Marker(this.formatLatLon(coordinates), this.LocationPoi);
+		
+			this.Map.markersLayer.addMarker(this.LocationMarker);
+			this.LocationPoi.setLocationAccuracy(accuracy);
+			this.LocationPoi.startLocatingAnimation();
+		} 
+		else {
+			/* don't use moveTo() because when the x,y = 0 
+			 * the marker actually moved to the old center of the map
+			 * !@#$!!
+			 * 
+			 * so we brutally set the lonlat manually
+			 */
+			this.LocationMarker.lonlat = this.formatLatLon(coordinates);
+			this.LocationPoi.setLocationAccuracy(accuracy);
+			
+			if(!stillLocating) this.LocationPoi.startLocatedAnimation();
+		}
+	},
+	
+	clearLocationPoi: function() {
+		if(this.LocationMarker != null) {
+			this.Map.markersLayer.removeMarker(this.LocationMarker);
+			this.LocationMarker = null;
+		}
+	},
+	
+	/* get the difference between the coords and the current map center in px */
+	getLocMapCtrPxDiff: function(coords) {
+		if(coords == null) return;
+
+		/* grab the location in px */
+		var userPos = this.Map.getPixelFromLonLat(this.formatLatLon(coords));
+		/* grab the center of the map in px */
+		var centerPos = this.Map.getPixelFromLonLat(this.Map.getCenter());
+		/* calculate the distance in px */
+		return new OpenLayers.Pixel(userPos.x - centerPos.x, userPos.y - centerPos.y);
+	},
+	
+	customInit: function(map) {
+		if(typeof(DeviceLocation) != 'undefined') {
+			this.LocationPoi = this.createLocationPoi(map);
+			MAP.DEVICELOCATION.instance = new DeviceLocation(null, MAP.DEVICELOCATION.AutoLocate);
+		}
+	}
+});
---
+++ src/web/uiresources/javascript/map-apple-android/comp/map/interactivePoi.js
@@ -0,0 +1,202 @@
+MobEMS.implement({
+	/* holds the dock */
+	Dock: null,
+	
+	/* holds the detected pois with the same lat lon */
+	MultiPois: new Array(),
+	
+	addPoi: function(map, marker, icon) {
+		var iconText = new String();
+		
+		/* if no type is set then go back to the old static image poi */
+		if(icon.type == '') {
+			return this.__addPoi(marker, icon);
+		}
+		
+		/* try to use number for SLIM / THICK poi 
+		 * these 2 types of poi will just truncate the text with elipsis
+		 * which means you'll only get "A..." if we use text 
+		 * We need to detect if app wants a SLIM/THICK poi with blank text 
+		 * (a fat dot is displayed instead) so if the app supply text == "."
+		 * then we'll treat it as if it asks for a blank poi of THICK/SLIM type
+		 */
+		else if(icon.type == 'SLIM' || icon.type == 'THICK') {
+			if(icon.text != ".") {
+				var iconText = this.grabNumberedTextFromUrl(icon.url);
+				/* if no number is found use the blank poi */
+				if(!iconText) iconText = ""; 
+			}
+			else iconText = "";
+		}
+		
+		/* for TEXT InteractiveIcon type set text */
+		else iconText = icon.text;
+			
+		var interactiveIcon = new EMS.InteractiveIcon(map,{
+			markerStyle:EMS.InteractiveMarkerStyles[icon.type], text:iconText, title:icon.title
+		}); 
+		var interactiveMarker = new OpenLayers.Marker(this.formatLatLon(marker.coordinates), interactiveIcon);
+		
+		this.Map.markersLayer.addMarker(interactiveMarker);
+		
+		/* add popup if possible */
+		if($(icon.id))
+			this.addPopup(icon.id, interactiveIcon, interactiveMarker);
+		
+		return interactiveMarker ;
+	},
+	
+	addPois: function(map, iconsList) {
+		var resultLength = iconsList.length;
+		if(resultLength > 0){			
+			var distinctPois = this.detectUniquePoi(iconsList);
+			var distinctPoisLength = distinctPois.length;
+			
+			for(var i = 0; i < distinctPoisLength; i++) {
+				/* construct the marker and icon */
+				var newMarker = {coordinates: iconsList[i].coordinates};
+				var newIcon = {
+						id: distinctPois[i].id,
+						title: distinctPois[i].title,
+						text: distinctPois[i].text,
+						type: distinctPois[i].type,
+						url: distinctPois[i].url,
+						width: distinctPois[i].width,
+						height: distinctPois[i].height,
+						offsetX: distinctPois[i].offsetX,
+						offsetY: distinctPois[i].offsetY
+					};
+				this.addPoi(map, newMarker, newIcon);
+			}
+		}
+	},
+	
+	detectUniquePoi: function(iconList) {
+		/* place holder for pois with unique coordinates */
+		var pois = new Array();
+		
+		for(var i = 0; i < iconList.length; i++) {
+			if(pois.length == 0) {
+				pois.push(iconList[i]);
+				continue;
+			}
+			
+			/* if current poi is a multi poi or has the same coords as another poi
+			 * that's already in the array, this poi will not have its own icon. Instead
+			 * it will modify the icon already in the list into a multi poi. 
+			 * 
+			 * A Multi poi default to SLIM_MULTI unless one of them is a THICK poi then
+			 * it will morph into MULTI
+			 */
+			var index = this.isPoiInArray(iconList[i], pois);
+			if(index) {
+				if(iconList[i].type == 'THICK') 
+					iconList[index].type = 'MULTI';
+				else {
+					/* only updates if the type is not multi yet */
+					if(iconList[index].type != 'MULTI')
+						iconList[index].type = 'SLIM_MULTI';
+				}
+				
+				/* push the current poi (iconList[i] to the multiPois list */
+				this.MultiPois.push(iconList[i]);
+				/* if the iconList[index] not in multiPois array then push it there */
+				if(!this.isPoiInArray(iconList[index], this.MultiPois)) {
+					this.MultiPois.push(iconList[index]);
+				}
+				
+				continue;
+			}
+			/* only push to pois when it is a unique poi */
+			else pois.push(iconList[i]);
+		}
+		
+		return pois;
+	},
+	
+	/* check if there's any other poi in the neighbourhood with the same lat-lon */
+	isPoiInArray: function(icon, iconList) {
+		for(var i = 0; i < iconList.length; i++) {
+			if(this.isLatLonEqual(icon.coordinates, iconList[i].coordinates)) {
+				return i;
+				break;
+			}
+		}
+		
+		return false;
+	},
+	
+	/* utility method to grab the poi number from the image url * */	
+	grabNumberedTextFromUrl: function(url) {
+		var pattern = '^(.*\/)+([0-9]+)\_(.*)+\.image$';
+		var filter = new RegExp(pattern, 'i');
+		var number = url.replace(filter, "$2");
+		
+		return number == url.length ? false : number;
+	},
+	
+	addPopup: function(id, icon, marker) {
+		/* extends the icon object with id, popup content, and marker */
+		icon.id = id;
+		icon.marker = marker; 
+		icon.popup = $(id);
+		
+		/* if no dock is specified */
+		if(this.Dock == null) {
+			/* create event to open popup */
+			icon.div.addEventListener('touchend', function() {
+				this.popupDiv = this.showPopup(this.popup, true);
+				/* create event to close popup */ 
+				this.popupDiv.addEventListener('touchend', function(){
+					this.hidePopup();
+				}.bind(this), false);
+			}.bind(icon), false);
+		} 
+		
+		else {
+			icon.div.addEventListener('touchend', function() {
+				this.loadDockWithContentFromIndex(this.getPopupIndexById(icon.id));
+			}.bind(this), false);
+		}
+	}, 
+
+	addDock: function(map, dOpt) {
+		/* instantiate */
+		this.Dock = new EMS.Control.DockedInfoBox();
+		/* if there's only 1 popup then use just that */
+		if($('mapPopup').getChildren().length == 1)
+			this.Dock.setContents(($('mapPopup').getChildren())[0], dOpt);
+		/* otherwise pass in the whole childNodes array */
+		else this.Dock.setContents($('mapPopup').getChildren(), dOpt);
+	
+		/* dock is a controller so add it to our map but we need to put it outside the viewport like anyother 
+		 * map controls 
+		 * 
+		 * try to detect if the app declare a dom with id = mapDockBox. If it is declared we'll
+		 * dump the box there. Otherwise it will be embedded to the map it self.
+		 * */
+		if($('mapDockBox'))
+			$('mapDockBox').appendChild(this.Dock.draw());
+		else map.div.appendChild(this.Dock.draw()); 
+	},
+	
+	getPopupIndexById: function(id) {
+		var popupContents = this.Dock.contents;
+		if(this.Dock.contents instanceof Array) {
+			var popupContentsLength = popupContents.length;
+			for(var i = 0; i < popupContentsLength; i++) {
+				if(popupContents[i].id == id) {
+					return i;
+					break;
+				}
+			}
+		}
+		return -1;
+	},
+	
+	loadDockWithContentFromIndex: function(idx) {
+		if(this.Dock.contents instanceof Array) {
+			this.Dock.loadContentsForIndex(idx);
+		}
+	}
+});
