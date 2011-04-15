b77e1d19af40f98e00fce709c26b662dc27fd208
w89477 <w89477>
2011-04-15 01:39:31 +0000

    locate me map control for iphone4 does similar thing as the other except it will half the image size

---
+++ src/web/uiresources/javascript/iphone4/comp/map/mobControl_locateMe.js
@@ -0,0 +1,13 @@
+EMS.Control.LocateMe = OpenLayers.Class(EMS.Control.LocateMePrototype, {
+	
+	calcDimension: function() {
+		/* set the used images sizes to half of it */
+		this.locateImage.height = this.locateImage.height / 2;
+		this.locateImage.width = this.locateImage.width / 2;
+		
+		return new OpenLayers.Size(this.locateImage.width, this.locateImage.height);
+	}
+	
+});
+
+_MapControls_.push(new EMS.Control.LocateMe); 
