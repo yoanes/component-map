a7d1ccf2b541d16bb61c55fb2f877a8a383ee958
w89477 <w89477>
2011-04-28 07:02:30 +0000

    put mobControl_THUB is now part of _MapControlsCompulsory_

--- src/web/uiresources/javascript/thub/comp/map/mobControl_THUB.js
+++ src/web/uiresources/javascript/thub/comp/map/mobControl_THUB.js
@@ -225,4 +225,4 @@ EMS.Control.MobileDefaults = OpenLayers.Class(OpenLayers.Control, {
 	}
 });
 
-_MapControls_.push(new EMS.Control.MobileDefaults);
+_MapControlsCompulsory_.push(new EMS.Control.MobileDefaults);
